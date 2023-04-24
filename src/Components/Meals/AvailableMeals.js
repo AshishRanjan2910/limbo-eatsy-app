9e04f27f9e1630d6fdc199042c169cd2960ef4d5
Ashish Ranjan <67757899+AshishRanjan2910@users.noreply.github.com>
2023-04-24 18:19:15 +0530

    Header and meals template changed

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card/Card";
+import MealItem from "./MealItem";
 
 const DummyList = [
   {
@@ -29,13 +31,23 @@ const DummyList = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DummyList.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DummyList.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-  )
+  );
 }
 
 export default AvailableMeals;
